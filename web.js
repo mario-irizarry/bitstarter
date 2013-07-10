ce392280b1768252bea28faee2acdb4ecbd3c124
Mario Irizarry <mirizarry@my.harrisburgu.edu>
2013-07-10 15:23:25 +0000

    Made changes to web.js

--- web.js
+++ web.js
@@ -9,7 +9,7 @@ var out = buffer.toString('utf8');
 var out = fs.readFileSync("index.html", 'utf8');
 */
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync('index.html', 'utf-8');
+  response.send(fs.readFileSync('index.html', 'utf8');
 });
 
 var port = process.env.PORT || 5000;
