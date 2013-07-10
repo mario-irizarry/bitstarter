7a9f0d00803e7df419572faab23a4d2952e14b09
Mario Irizarry <mirizarry@my.harrisburgu.edu>
2013-07-10 15:55:12 +0000

    Made corrections to web.js

--- web.js
+++ web.js
@@ -3,12 +3,12 @@ var fs=require('fs');
 
 var app = express.createServer(express.logger());
 
-var buffer = new Buffer(28);
-buffer.write(fs.readFileSync("./index.html", 'utf8');
+var buffer = new Buffer("Hello out there!", "utf8");
+
 var out = buffer.toString('utf8');
 
 app.get('/', function(request, response) {
-  response.send("Hi, I am here!");
+  response.send(out);
 });
 
 var port = process.env.PORT || 5000;
