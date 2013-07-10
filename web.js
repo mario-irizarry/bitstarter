fe97bdbce1e797b7fe8075169639098e1aaed668
Mario Irizarry <mirizarry@my.harrisburgu.edu>
2013-07-10 15:31:33 +0000

    Made corrections to web.js

--- web.js
+++ web.js
@@ -1,12 +1,12 @@
-var express = require('express');
+var express=require('express'), fs=require('fs');
 
 var app = express.createServer(express.logger());
-/*
+
 var buffer = new Buffer(27);
-buffer.write(fs.readFileSync("index.html", "utf8");
+buffer.write(fs.readFileSync("index.html");
 var out = buffer.toString('utf8');
-*/
-var out = fs.readFileSync('index.html', 'utf8');
+
+//var out = fs.readFileSync('index.html', 'utf8');
 
 app.get('/', function(request, response) {
   response.send(out);
