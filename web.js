7252efbde7d8cd9107cdb518082c641f4f090114
Mario Irizarry <mirizarry@my.harrisburgu.edu>
2013-07-10 15:11:36 +0000

    Made changes to web.js, replaced buffer.

--- web.js
+++ web.js
@@ -1,10 +1,12 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-
+/*
 var buffer = new Buffer(27);
 buffer.write(fs.readFileSync("index.html", "utf8");
 var out = buffer.toString('utf8');
+*/
+var out = fs.readFileSync("index.html", "utf8");
 
 app.get('/', function(request, response) {
   response.send(out);
