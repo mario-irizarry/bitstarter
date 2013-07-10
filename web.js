4443927e3bb5da2fc42c9a11355eb6a7edfca44c
Mario Irizarry <mirizarry@my.harrisburgu.edu>
2013-07-10 15:25:01 +0000

    Made changes to web.js

--- web.js
+++ web.js
@@ -5,11 +5,11 @@ var app = express.createServer(express.logger());
 var buffer = new Buffer(27);
 buffer.write(fs.readFileSync("index.html", "utf8");
 var out = buffer.toString('utf8');
-
-var out = fs.readFileSync("index.html", 'utf8');
 */
+var out = fs.readFileSync("index.html", 'utf8');
+
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync('index.html', 'utf8');
+  response.send(out);
 });
 
 var port = process.env.PORT || 5000;
